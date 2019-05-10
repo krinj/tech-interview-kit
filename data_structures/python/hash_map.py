9b56a528921d573d3e18d4bfe4f7e2f08f3120ba
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2019-05-10 19:23:43 +0800

    Implemented Python Hash Map

--- data_structures/python/hash_map.py
+++ data_structures/python/hash_map.py
@@ -1,12 +1,96 @@
-class HashMap:
+from typing import List
+
+
+class HashNode:
     def __init__(self):
-        pass
+        self.key = None
+        self.val = None
+        self.next: HashNode = None
+
+
+class HashMap:
+
+    def __init__(self):
+        self._storage: List[HashNode] = []
+        self._capacity = 0
+        self._limit = 0  # When the capacity reaches this, then resize the array.
+        self._size = 0
+        self.resize(8)
+
+    def resize(self, new_size: int):
+        old_capacity = self._capacity
+
+        # Increase the capacity.
+        new_storage = []
+        for _ in range(new_size):
+            new_storage.append(HashNode())
+
+        self._capacity = new_size
+        self._limit = int(self._capacity * 0.7)
+
+        # Move the items.
+        for i in range(old_capacity):
+            node = self._storage[i]
+            while node.key is not None:
+                self._set_item_to_array(node.key, node.val, new_storage)
+                node = node.next
+
+        # Set the storage to the new.
+        self._storage = new_storage
+
+    @staticmethod
+    def get_hash_index(key, capacity):
+        return hash(key) % capacity
 
     def __getitem__(self, key):
-        return key
+        index = self.get_hash_index(key, len(self._storage))
+        node = self._storage[index]
+        while node.key != key:
+            node = node.next
+            if node is None:
+                raise Exception(f"Key {key} not found in Hash Table.")
+        return node.val
 
     def __setitem__(self, key, value):
-        return key
+
+        if key is None:
+            raise Exception("Cannot use None for key.")
+        self._set_item_to_array(key, value, self._storage)
+
+        self._size += 1
+        if self._size >= self._limit:
+            self.resize(int(self._capacity * 1.5))
+
+    def _set_item_to_array(self, key, value, array):
+        index = self.get_hash_index(key, len(array))
+        node = array[index]
+
+        while node.key is not None:
+            node = node.next
+
+        node.key = key
+        node.val = value
+        node.next = HashNode()
 
     def size(self):
-        return 0
+        return self._size
+
+    def display(self):
+
+        for i in range(self._capacity):
+            display_line = [f"{str(i).zfill(3)}: ["]
+            node = self._storage[i]
+            while node.key is not None:
+                display_line.append(f"{node.key} ({node.val})")
+
+                if node.next.key is not None:
+                    display_line.append(" -> ")
+
+                node = node.next
+
+            display_line.append("]")
+            print("".join(display_line))
+
+        print(f"\nCapacity: {self._capacity}")
+        print(f"Size: {self._size}")
+        print(f"Limit: {self._limit}")
--- data_structures/python/tests/test_hash_map.py
+++ data_structures/python/tests/test_hash_map.py
@@ -7,6 +7,55 @@ class TestHashMap(unittest.TestCase):
     def setUp(self):
         self.hash_map = HashMap()
     
-    def test_insert(self):
-        print(self.hash_map)
-        pass
+    def test_set_item(self):
+        self.hash_map["A"] = 1
+        self.hash_map["B"] = 2
+        self.hash_map["C"] = 3
+        self.hash_map["D"] = 4
+        self.hash_map["E"] = 5
+        self.hash_map["F"] = 6
+        self.hash_map["G"] = 7
+        self.hash_map["H"] = 8
+
+        self.assertEqual(8, self.hash_map.size())
+
+    def test_get_item(self):
+        self.hash_map["A"] = 1
+        self.hash_map["B"] = 2
+        self.hash_map["C"] = 3
+        self.hash_map["D"] = 4
+        self.hash_map["E"] = 5
+        self.hash_map["F"] = 6
+        self.hash_map["G"] = 7
+        self.hash_map["H"] = 8
+
+        self.assertEqual(1, self.hash_map["A"])
+        self.assertEqual(2, self.hash_map["B"])
+        self.assertEqual(3, self.hash_map["C"])
+        self.assertEqual(4, self.hash_map["D"])
+        self.assertEqual(5, self.hash_map["E"])
+        self.assertEqual(6, self.hash_map["F"])
+        self.assertEqual(7, self.hash_map["G"])
+        self.assertEqual(8, self.hash_map["H"])
+
+    def test_resize(self):
+        self.hash_map["A"] = 1
+        self.hash_map["B"] = 2
+        self.hash_map["C"] = 3
+        capacity = self.hash_map._capacity
+
+        self.hash_map["D"] = 1
+        self.hash_map["E"] = 2
+        self.hash_map["F"] = 3
+        self.assertGreater(self.hash_map._capacity, capacity)
+
+    def test_display(self):
+        self.hash_map["A"] = 1
+        self.hash_map["B"] = 2
+        self.hash_map["C"] = 3
+        self.hash_map["D"] = 4
+        self.hash_map["E"] = 5
+        self.hash_map["F"] = 6
+        self.hash_map["G"] = 7
+        self.hash_map["H"] = 8
+        self.hash_map.display()
